b345dfd2f82364f992bd564af8a0f9bb12e94de2
dsadaskwq <111348232+dsadaskwq@users.noreply.github.com>
2024-11-26 01:20:54 +0800

    Update diy-part1.sh

--- JDCloud-AX6000-Baili-hanwckf/diy-part1.sh
+++ JDCloud-AX6000-Baili-hanwckf/diy-part1.sh
@@ -17,4 +17,15 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #git clone https://github.com/messense/aliyundrive-webdav package/messense
+rm -rf $(find ./feeds/packages/ -type d -regex ".*\(golang\|chinadns-ng\|sing-box\|xray-core\|v2ray-core\|v2ray-plugin\|v2ray-geodata\).*")
+
+git clone --depth=1 --single-branch https://github.com/sbwml/packages_lang_golang.git feeds/packages/lang/golang
+
+git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
+git clone https://github.com/vernesong/OpenClash package/OpenClash
+git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
+git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+git clone https://github.com/chenmozhijin/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/sirpdboy/netspeedtest package/netspeedtest
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
